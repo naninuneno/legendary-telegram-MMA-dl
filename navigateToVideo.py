536b8c9c6384425f7483722f56d81ad131d2796a
nbrowning <n.lalilulelo@gmail.com>
2017-06-10 11:11:05 +0100

    Better video finding so can find older vids

--- navigateToVideo.py
+++ navigateToVideo.py
@@ -7,17 +7,21 @@ if len(sys.argv) != 3:
   print "Please provide 2 fighter names"
   sys.exit()
 
-fighters = [sys.argv[1].lower(), sys.argv[2].lower()]
+queryTerms = [sys.argv[1].lower(), sys.argv[2].lower(), "fight", "video"]
+query = "+".join(queryTerms)
   
-response = requests.get("http://mmaversus.com")
+response = requests.get("http://mmaversus.com/?s=" + query)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 firstUrl = None
 for link in soup.find_all('a'):
   url = link.get('href').lower()
-  if all(str in url for str in fighters):
+  if all(str in url for str in queryTerms):
     firstUrl = link.get('href')
     break
+    
+if firstUrl == None:
+  print "Couldn't find video"
     
 response = requests.get(firstUrl)
 soup = BeautifulSoup(response.text, 'html.parser')
@@ -26,6 +30,9 @@ desiredUrl = None
 for link in soup.find_all('a'):
   if 'Dedi.Blog' in link.getText():
     desiredUrl = link.get('href')
+    
+if desiredUrl == None:
+  print "Couldn't find video"
     
 sys.stdout.write(desiredUrl)
 sys.stdout.flush()
